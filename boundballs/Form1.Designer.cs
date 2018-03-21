57e333f7bc69225695fde91db625c6241f86726e
Bin Ichiki <bix191@gmail.com>
2018-03-21 20:03:06 +0900

    プロジェクト ファイルを追加します。

---
+++ boundballs/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace boundballs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1294, 552);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1318, 616);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ boundballs/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+
+namespace boundballs
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        BounsBall[] bb;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream stream = assembly.GetManifestResourceStream("boundballs.Resources.ball.bmp");
+            bmp = new Bitmap(stream);
+            bmp.MakeTransparent(Color.FromArgb(255, 255, 255));
+
+            Random r = new Random();
+
+            bb = new BounsBall[100];
+            for (int i = 0; i < bb.Length; i++)
+            {
+                bb[i] = new BounsBall(bmp.Size.Width, bmp.Size.Height,
+                    r.Next(panel1.Size.Width - bmp.Size.Width), r.Next(panel1.Size.Height - bmp.Size.Width));
+
+            }
+
+            timer1.Start();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            bmp.SetResolution(e.Graphics.DpiX, e.Graphics.DpiY);
+            for (int i = 0; i < bb.Length; i++)
+            {
+                e.Graphics.DrawImage(bmp,
+                    new Point(bb[i].getX(), bb[i].getY()));
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < bb.Length; i++)
+            {
+                bb[i].move(panel1.Size.Width, panel1.Size.Height);
+            }
+
+            panel1.Invalidate();
+        }
+    }
+    class BounsBall
+    {
+        int x, y;
+        int bw, bh;
+        int dx, dy;
+        static Random r = new Random();
+        public BounsBall(int _bw, int _bh, int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+            bw = _bw;
+            bh = _bh;
+            int[] d = { -10, -8, -6, -4, -2, 2, 4, 6, 8, 10 };
+            //int[] d = { 0,0,0,0,0, 0,0,0,0,0  };
+
+            dx = d[r.Next(10)];
+            dy = d[r.Next(10)];
+        }
+
+        public void move(int width, int height)
+        {
+            if (x < 0)
+            {
+                dx *= -1;
+            }
+            else if (x > width - bw)
+            {
+                dx *= -1;
+            }
+            if (y < 0)
+            {
+                dy *= -1;
+            }
+            else if (y > height - bh)
+            {
+                dy *= -1;
+            }
+            x += dx;
+            y += dy;
+        }
+        public int getX()
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+
+    }
+}
